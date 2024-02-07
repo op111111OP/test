ac94a5dc991ea309618ddb07fe2856f0f89845ff
op111111OP <p47601758@gmail.com>
2024-02-07 14:41:11 +0200

    renamed:    src/app/context/page.js -> src/app/Context/store.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 // import Footer from "../components/Footer/Footer";
 
 const inter = Montserrat({ subsets: ["latin"] });
-import { UserProvider } from "./context/page";
+import { UserProvider } from "./Context/store";
 
 export const metadata = {
   title: "",
