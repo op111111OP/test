51ea16c23c5aa7660873f4444120c396f1677483
op111111OP <p47601758@gmail.com>
2024-02-07 14:21:54 +0200

    modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 // import { useEffect, useState } from "react";
 // import Image from "next/image";
-// import styles from "./page.module.css";
 // import Swipe from "../components/Swiper/Swipe";
 // import SwipeNovelty from "../components/SwiperNovelty/SwiperNovelty";
 // import SwiperBrand from "../components/SwiperBrand/SwiperBrand";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
