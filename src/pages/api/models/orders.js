f98275bda9ffc95f9704405c9b37594ff30120af
op111111OP <p47601758@gmail.com>
2024-02-07 14:32:58 +0200

    deleted:    src/pages/api/find.js

--- src/pages/api/models/orders.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
-const ordersSchema = new Schema({});
-
-const Orders =
-  mongoose.models.Orders || mongoose.model("Orders", ordersSchema, "categori");
-
-export default Orders;
