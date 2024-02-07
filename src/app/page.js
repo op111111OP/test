333b0a0bbc5db22abd969e86f505ef459ec18b57
op111111OP <p47601758@gmail.com>
2024-02-07 13:54:13 +0200

    deleted:    src/Context/store.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
 "use client";
 // import { useEffect, useState } from "react";
 // import Image from "next/image";
-import styles from "./page.module.css";
-import Swipe from "../components/Swiper/Swipe";
-import SwipeNovelty from "../components/SwiperNovelty/SwiperNovelty";
-import SwiperBrand from "../components/SwiperBrand/SwiperBrand";
+// import styles from "./page.module.css";
+// import Swipe from "../components/Swiper/Swipe";
+// import SwipeNovelty from "../components/SwiperNovelty/SwiperNovelty";
+// import SwiperBrand from "../components/SwiperBrand/SwiperBrand";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Swipe />
+      {/* <Swipe />
       <SwipeNovelty />
-      <SwiperBrand />
+      <SwiperBrand /> */}
     </main>
   );
 }
